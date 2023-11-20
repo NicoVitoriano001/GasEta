8cc7b0573c45ae605969cc5895bcb510033bbbb5
NicoVitoriano001 <nicovitoriano@gmail.com>
2023-11-20 20:33:20 -0300

    GasEta - Reformulação do layout para adaptar ao gaseta 3

---
+++ app/src/main/java/com/example/appgaseta/apoio/UtilGasEta.java
@@ -0,0 +1,16 @@
+package com.example.appgaseta.apoio;
+
+public class UtilGasEta {
+
+    public void metodoEstatico(){
+//para utilizar, vai no MainActivity
+    }
+
+    public static void metodoNaoEstatico(){}
+
+    public static String mensagem(){
+        return "Qualquer Mensagem";
+
+    }
+
+}
--- app/src/main/java/com/example/appgaseta/view/GasEtaActivity.java
+++ app/src/main/java/com/example/appgaseta/view/GasEtaActivity.java
@@ -1,17 +1,26 @@
 package com.example.appgaseta.view;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.appgaseta.R;
+import com.example.appgaseta.apoio.UtilGasEta;
 
 public class GasEtaActivity extends AppCompatActivity {
+
+   // UtilGasEta utilGasEta = new UtilGasEta();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gaseta);
+
+        UtilGasEta.metodoNaoEstatico();
+
+       Toast.makeText(GasEtaActivity.this, UtilGasEta.mensagem(),Toast.LENGTH_LONG).show();
+
     }
 }

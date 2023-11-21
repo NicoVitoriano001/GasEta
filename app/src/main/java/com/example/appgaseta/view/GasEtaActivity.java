30639e2a81a95aa38436a97baa04bc156a12bec1
NicoVitoriano001 <nicovitoriano@gmail.com>
2023-11-20 21:52:47 -0300

    GasEta - Reformulação do layout para adaptar ao gaseta 5

--- app/src/main/java/com/example/appgaseta/view/GasEtaActivity.java
+++ app/src/main/java/com/example/appgaseta/view/GasEtaActivity.java
@@ -1,6 +1,10 @@
 package com.example.appgaseta.view;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -10,25 +14,68 @@ import com.example.appgaseta.R;
 import com.example.appgaseta.apoio.UtilGasEta;
 
 public class GasEtaActivity extends AppCompatActivity {
-
-   // UtilGasEta utilGasEta = new UtilGasEta();
+    EditText editGasolina;
+    EditText editEtanol;
+    TextView txtResultado;
+    Button btnCalcular;
+    Button btnLimpar;
+    Button btnSalvar;
+    Button btnFinalizar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gaseta);
 
-        UtilGasEta.metodoNaoEstatico();
+        editGasolina = findViewById(R.id.editGasolina);
+        editEtanol = findViewById(R.id.editEtanol);
 
-       Toast.makeText(GasEtaActivity.this,
-               UtilGasEta.calcularMelhorOpcao(5.12, 3.39),
-               Toast.LENGTH_LONG).show();
-  }
+        txtResultado = findViewById(R.id.txtResultado);
 
-  public static double preco(){
+        btnCalcular = findViewById(R.id.btnCalcular);
+        btnSalvar = findViewById(R.id.btnSalvar);
+
+        btnLimpar = findViewById(R.id.btnLimpar);
+        btnFinalizar = findViewById(R.id.btnFinalizar);
+
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(GasEtaActivity.this, "VolteXXXXX sempre", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+
+
+        Toast.makeText(GasEtaActivity.this,
+                UtilGasEta.calcularMelhorOpcao(5.12, 3.39),
+                Toast.LENGTH_LONG).show();
+    }
+
+    public static double preco() {
         return 9.99;
-  }
-
+    }
 
 
 }
--- app/src/main/java/com/example/appgaseta/view/MainActivity.java
+++ app/src/main/java/com/example/appgaseta/view/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     CursoController cursoController;
     Pessoa pessoa;
     List<String> nomesDosCuros;
+
     Pessoa outrapessoa;
     String dadosPessoa;
     String dadosOutraPessoa;
